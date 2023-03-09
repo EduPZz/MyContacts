c9b7f8d525f369c18ac79d32c04c2d3672c19caa
EduPZz <eduardo.pozzogood@hotmail.com>
2023-03-09 18:36:52 +0000

    Criando os componentes Input e Select

--- fe/src/components/PageHeader/styles.js
+++ fe/src/components/PageHeader/styles.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   a {
+    display: flex;
+    align-items: center;
     text-decoration: none;
+
     span {
       font-weight: bold;
       color: ${({ theme }) => theme.colors.primary.main};
@@ -11,5 +14,9 @@ export const Container = styled.header`
       margin-right: 8px;
       transform: rotate(-90deg);
     }
+  }
+
+  h1 {
+    font-size: 24px;
   }
 `;
